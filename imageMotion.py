72578ff3dccfadff75df9dc73fcb1a8e7d17dbbe
Ridhi Surana <ridhisurana@Ridhis-Air.wv.cc.cmu.edu>
2016-01-13 21:06:56 -0500

    Corrected the co-ordinate system

--- imageMotion.py
+++ imageMotion.py
@@ -7,10 +7,8 @@ from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 # Assuming we get an image of size 64X64
 
-upper15 = 48
-left15  = 48
-bottom15 = 64
-right15  = 64
+
+sList = [3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13 ,12]
 
 
 def main():
@@ -111,30 +109,38 @@ def image():
     newIm = putBorders(blacken15(im))
     newIm.show()
     tileList = getTileList(newIm)
-    scrambleImage(tileList,im)
+    scIm = scrambleImage(tileList,im,sList)
+    scIm.show()
+    hgIm = highlightOnSelect(1,2,scIm)
+    hgIm.show()
     return None
 
 def putBorders(im):
-    row,col = im.size   
-    height = col
-    width  = row
-    # height = width
-    increment = height/4
-    startVal = width/4
-    while(startVal < width):
-        for i in range(height):
-            im.putpixel((startVal,i), (0,0,0))
-        startVal += increment
-    startVal = height/4
-    while(startVal < height):
-        for i in range(height):
-            im.putpixel((i,startVal), (0,0,0))
-        startVal += increment
-    tileList = getTileList(im)
+    width,height = im.size   
+    inc = height/4
+    # putting borders vertically
+    startX = width/4
+    while(startX < width):
+        for y in xrange(height):
+            im.putpixel((startX,y),(0,0,0))
+        startX += inc
+    # putting borders horizontally
+    startY = height/4
+    while(startY < height):
+        for x in xrange(width):
+            im.putpixel((x,startY),(0,0,0))
+        startY += inc
     return im
     
 def blacken15(im):
-    tile15 = (upper15,left15,bottom15,right15)
+    width,height = im.size
+    # square images only
+    inc = height/4
+    x0 = width - inc
+    y0 = height - inc
+    x1 = width
+    y1 = height
+    tile15 = (x0,y0,x1,y1)
     region = im.crop(tile15)
     row,col = region.size
     for i in range (row):
@@ -144,58 +150,92 @@ def blacken15(im):
     return im
 
 def getTileList(im):
-    upper = 0
-    left  = 0
-    height,width = im.size
+    x0 = 0
+    y0 = 0
+    width,height = im.size
     inc = height/4
     tileList = [ 0 for i in range(16)]
     i = 0
-    while(left < height):
-        while(upper < width):
-            bottom = upper + inc
-            right  = left + inc
-            tile = im.crop((upper,left,bottom,right))
+    # go vertically
+    while(y0 < height):
+        # go horizontally
+        while( x0 < width):
+            x1 = x0 + inc
+            y1 = y0 + inc
+            tile = im.crop((x0,y0,x1,y1))
             tileList[i] = tile
             i += 1
-            upper += inc
-        upper = 0
-        left += inc
+            x0 += inc
+        x0 = 0
+        y0 += inc
     return tileList
 
-def scrambleImage(tileList,im):
-    scList = [3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13 ,12]
-    row,col = im.size
-    upper = 0
-    left = 0
-    inc = row/4
-    im = Image.new('RGB', (row,col))
+def scrambleImage(tileList,im,scList):
+    
+    # tileList = a list representing the current
+    # tiles in form of a list. Each tile is a 
+    # sub-image of 16X16
+    # scList is the list of indices reprsenting the 
+    # order of tiles to appear in the scrambled image
+    width,height = im.size
+    x0 = 0
+    y0 = 0
+    inc = height/4
+    # pasting on an empty image
+    im = Image.new('RGB', (width,height))
+    # pasting tiles row-wise
     for i in range(len(scList)):
+        # getting the index
         index = scList[i]
+        # getting the tile to paste
         tile = tileList[index]
-        if(upper == 3*inc):
-            bottom = upper + inc
-            right = left + inc
-            box = (upper,left,bottom,right)
-            upper += inc
+        if(x0 == 3*inc):
+            x1 = x0 + inc
+            y1 = y0 + inc
+            box = (x0,y0,x1,y1)
             im.paste(tile,box)
-            upper = 0
-            left += inc
-            
+            x0 = 0
+            y0 += inc
         else:
-            bottom = upper + inc
-            right = left + inc
-            box = (upper,left,bottom,right)
-            upper += inc
+            x1 = x0 + inc
+            y1 = y0 + inc
+            box = (x0,y0,x1,y1)
             im.paste(tile,box)
+            x0 += inc
+    return im
 
-    im.show()
+def highlightOnSelect(tileX,tileY,im):
+    width,height = im.size
+    inc = height/4
+    startX = tileY * inc  #(2I)
+    startY  = tileX * inc  #(I)
+    endX = startX + inc
+    endY = startY + inc
+    
+    # highlight both vertical
+    # sides of the tile
+    while(startX < endX):
+        for y in xrange(inc):
+            im.putpixel((startX,startY + y),(255,255,255))
+        startX += inc - 1
+    
+    # highlight both horizontal 
+    # sides of the tile
+    startX = tileY * inc
+    while(startY < endY):
+        for x in xrange(inc):
+            im.putpixel((startX + x, startY), (255,255,255))
+        startY += inc - 1
+        
+    return im
 
 
 
 
+  
 
 
 
 
-#image()
+image()
 main()
